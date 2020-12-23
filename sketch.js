3fca617f1e8d748f75f8034ad804c8f8da4df238
Daniel3430 <72322428+Daniel3430@users.noreply.github.com>
2020-12-23 18:37:00 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,56 +1,44 @@
-var dog,dogImg,dogImg1;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
 var database;
-var foodS,foodStock;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-   dogImg=loadImage("Images/Dog.png");
-   dogImg1=loadImage("Images/happy dog.png");
+  track = loadImage("track.jpg");
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  car3_img = loadImage("car3.png");
+  car4_img = loadImage("car4.png");
+  ground = loadImage("ground.png");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
   }
-
-//Function to set initial environment
-function setup() {
-  database=firebase.database();
-  createCanvas(500,500);
-
-  dog=createSprite(250,300,150,150);
-  dog.addImage(dogImg);
-  dog.scale=0.15;
-
-  foodStock=database.ref('Food');
-  foodStock.on("value",readStock);
-  textSize(20); 
-}
-
-// function to display UI
-function draw() {
-  background(46,139,87);
- 
-  if(keyWentDown(UP_ARROW)){
-    writeStock(foodS);
-    dog.addImage(dogImg1);
+  if(gameState === 1){
+    clear();
+    game.play();
   }
-
-  drawSprites();
-  fill(255,255,254);
-  stroke("black");
-  text("Food remaining : "+foodS,170,200);
-  textSize(13);
-  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
-}
-
-//Function to read values from DB
-function readStock(data){
-  foodS=data.val();
-}
-
-//Function to write values in DB
-function writeStock(x){
-  if(x<=0){
-    x=0;
-  }else{
-    x=x-1;
-  } 
-  database.ref('/').update({
-    Food:x
-  })
+  if(gameState === 2){
+    game.end();
+  }
 }
